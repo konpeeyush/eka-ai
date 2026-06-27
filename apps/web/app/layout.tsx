99e7dcdbebe87da512797953a0a8c14a9d7e2079
konpeeyush <piyushksingh2589@gmail.com>
2026-06-27 13:06:57 +0530

    feat: implement authentication and organization selection flows with guards and views

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
         <ThemeProvider>
           <Providers>
             <ClerkProvider>
-            <ConvexClientProvider>
-              {children}
-            </ConvexClientProvider>
+              <ConvexClientProvider>
+                  {children}
+              </ConvexClientProvider>
             </ClerkProvider>
           </Providers>
         </ThemeProvider>
